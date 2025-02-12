66bca200c6e8579237ed7c931ddee17a2398f705
Em D <13535330+m-dwyer@users.noreply.github.com>
2025-02-12 21:18:19 +1100

    add neovim and displays with separate spaces to darwin script

--- run_once_darwin.sh
+++ run_once_darwin.sh
@@ -2,3 +2,6 @@
 
 # Disable wallpaper to reveal desktop
 defaults write com.apple.WindowManager "EnableStandardClickToShowDesktop" -bool "false" && killall WindowManager
+
+# Displays have separate spaces -- required for yabai to run (for hiding Mac menu bar)
+defaults write com.apple.spaces "spans-displays" -bool "true" && killall SystemUIServer
